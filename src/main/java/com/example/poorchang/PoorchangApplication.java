80d6720207af223a91db979adddd63ae0c7ccb2a
NHclub <sbe4488@naver.com>
2023-10-18 15:43:55 +0900

    init commit

---
+++ src/main/java/com/example/poorchang/PoorchangApplication.java
@@ -0,0 +1,13 @@
+package com.example.poorchang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoorchangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoorchangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poorchang/PoorchangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poorchang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoorchangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
